4ad37db1c875a86f8ce4d90ea9f7db2f6696e8ee
Szaniszló Árpád (MArpadszF) <developedbybarley@gmail.com>
2024-08-29 13:35:54 +0200

    Dynamic routes, PATCH, DELETE

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -3,6 +3,7 @@ import { comments } from './data'
 export async function GET() {
     return Response.json(comments)
 }
+
 export async function POST(request: Request) {
     const comment = await request.json();
 
